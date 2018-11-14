3400d13ee0c564052fa612d66d8376d372ec5a24
AezioxDev <44977477+AezioxDev@users.noreply.github.com>
2018-11-14 19:21:24 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,24 +3,57 @@ const bot = new Discord.Client();
 const prefix = ("?");
 
 
+    //-----------------------------------------------------------------------------------------------------------------
 
+//kick
+if(command === "kick") {
+    
+    if(!message.member.roles.some(r=>["Admin", "Modo"].includes(r.name)) )
+      return message.reply("Tu n'as pas la permission de faire ceci!");
+    
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Merci de spécifier un nom d'utilisateur valide");
+    if(!member.kickable) 
+      return message.reply("Vous ne pouvez pas expulsez cet utilisateur ! As-t-il un rôle supérieur ? Ai-je les permissions d'expulser un membre?");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "Aucune raison spécifiée";
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Désolé ${message.author} je ne peux pas expulser la personne car : ${error}`));
+    message.reply(`${member.user.tag} a été expulsé par ${message.author.tag} car : ${reason}`);
+
+    //-----------------------------------------------------------------------------------------------------------------
+    
+//ReadyPreset
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
     bot.user.setActivity('En développement').catch(console.error)
 });
+    
+   
+    //-----------------------------------------------------------------------------------------------------------------
 
+//Reboot
 bot.on('message', message => {
     if (message.content === 'rebootexec') {
       message.reply('Reboot exécuté ! Le bot va redemarrer !')
        message.reply('Le BOT à redémarré !')
     }
   })
+    
+    //-----------------------------------------------------------------------------------------------------------------
 
+//Message de bienvenue
 bot.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
     return channel.send('Bienvenue sur le serveur Albert de Mun' + member.displayName)
   }).catch(console.error)
     
+    //-----------------------------------------------------------------------------------------------------------------
+
+//Musique    
 })
 
 bot.on('message', message => {
@@ -51,7 +84,10 @@ bot.on('message', message => {
   }
 
 })
+    
+    //-----------------------------------------------------------------------------------------------------------------
 
+//Bonjour et site
 bot.on('message', msg => {
     if (msg.content === "bonjour"){
         msg.reply("Heureux de te revoir parmis nous.")

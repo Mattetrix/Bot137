0dc63ad1b79f70aadd34adf18d993364b4a841e8
AezioxDev <44977477+AezioxDev@users.noreply.github.com>
2018-11-14 18:46:16 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const prefix = ("?");
+
+
+
+bot.on('ready', function () {
+    console.log("Je suis prêt à être utilisé.")
+    bot.user.setActivity('rien').catch(console.error)
+});
+
+bot.on('message', message => {
+    if (message.content === 'ping') {
+      message.reply('pong !')
+    }
+  })
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        console.log(`${member.displayName} à rejoint le serveur.`)
+    }).catch(console.error)
+});
+
+
+
+bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
+    if (msg.content === prefix + "site"){
+        msg.channel.send("www.google.com")
+        console.log("Une personne a demandé pour aller sur ton site.")
+    }
+
+});
+
+bot.login(codetest);
